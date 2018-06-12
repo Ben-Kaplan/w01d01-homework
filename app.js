9b7f18239a329402f1d9fca7f710cc7899a128de
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-06-12 08:15:34 -0600

    homework returned

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
-alert("hello");
+//Section 1 
+
+//1: Interpolation corresponding values into placeholders. Concatenation is stringing strings together into statements using + signs.
+//concatenation
+var = red
+
+"My " + red + " wagon";
+
+//interpolation
+
+//2: Do not repeat yourself. Don't repeat code unnecessarily always use methods that remove repetetive code.
+
+//Section 2
+
+
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' === 'Name');
+console.log(a + b === c);
+console.log(a * a === d);
+console.log(e === 'Kevin');
+console.log(48 !== '48');
+
+
+//Section 3
+
+//infinite Loop?
+
+// yes
+
+//Inifite loop 2
+
+//no it would never run
+
+//while loop would log 20 A's
+
+//for loops accomplish what a while loop does but with a variable and operator contained with in it and on one line of code.
+
+//(the variable; the condition; the counter);
+
+//Section 4
+
+//loop in reverse
+
+for (i = 999; i >= 0; i--) {
+ console.log(i)
+}
+
+//More counting
+
+for (i = 1; i <= 10; i++) {
+
+console.log ("The value of i is: " + i + " of 10");
+}
+
+
+//Section 5
+
+
+
